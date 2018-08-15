3497a299fea72cf5bf9109a504708a31a1de22db
Sergey Shashkov <sh57@yandex.ru>
2018-08-15 15:03:18 +0300

    Часть русификации и исправления

--- flake8ejudge.py
+++ flake8ejudge.py
@@ -4,78 +4,103 @@ import sys
 import os
 import subprocess
 import re
+from flake8ejudgeformatter import LINE_STARTER
 
 FLAKE = 'flake8'
 MAX_LEN = 160
-IGNORE = 'W293,W292,W291,W391,F405,E722,E741,E743'
+IGNORE = 'W293,W292,W291,W391,F405,E722,E741,E743,E101,F403'
 TIMEOUT = 5
-
-# ejudge вызывает валидатор с единственным параметром — именем файла, которому необходима проверка.
-# src_name = sys.argv[1]
-
-src_name = r'X:\_все py\allpy\000811_139_SV_20508_4.py'
-
-errors_found = False
-# Если имя файла не заканчивается на .py, то игнорируем
-if not src_name.lower().endswith('.py'):
-    sys.exit()
-# Проверяем, что это действительно файл и у нас есть к нему доступ
-if not os.path.isfile(src_name):
-    sys.exit()
-# Дополняем стандартные параметры значениями из env
-# Заменяем все "-" на "_" и убираем ведущие "--", если они были
-evs = {key: val.replace("-", '_').lstrip('_') for key, val in os.environ.items()}
-flake_parms = [
-    '--show-source',
-    '--jobs=1',
-    '--isolated',
-    '--format=flake8ejudgeformatter'
-]
-if 'flake8_ignore' in evs:
-    flake_parms.append('--ignore=' + evs['flake8_ignore'])
-else:
-    flake_parms.append('--ignore=' + IGNORE)
-
-if 'max_line_length' in evs:
-    flake_parms.append('--max-line-length=' + evs['max_line_length'])
-else:
-    flake_parms.append('--max-line-length=' + str(MAX_LEN))
-
-# Запускаем flake
-pr = subprocess.Popen([FLAKE] + flake_parms + [src_name],
-                      stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)  # Запускаем процесс
-stdout_data_b, stderr_data_b = pr.communicate(timeout=TIMEOUT)  # Передаём данные в StdIN
-if stderr_data_b:
-    sys.stderr.write('flake8 error:\n' + stderr_data_b.decode('utf-8', 'ignore'))
-    sys.exit(1)
-if stdout_data_b:
-    stdout_data = stdout_data_b.decode('utf-8', 'ignore')
-    errors_found = True
-    sys.stderr.write(stdout_data)
+MAX_COMPLEXITY = 9
+MAX_ERRORS_TO_SHOW = 10
 
 
-# Читаем исходники
-try:
-    with open(src_name, 'r', encoding="utf-8", errors='ignore') as f:
-        source = f.read()
-except:
-    sys.stderr.write('Can not parse source with encoding utf-8')
-    sys.exit(1)
+def main(src_name: str, f_obj):
+    errors_found = False
+    # Если имя файла не заканчивается на .py, то игнорируем
+    if not src_name.lower().endswith('.py'):
+        return 0
+    # Проверяем, что это действительно файл и у нас есть к нему доступ
+    if not os.path.isfile(src_name):
+        return 0
+    # Дополняем стандартные параметры значениями из env
+    # Заменяем все "-" на "_" и убираем ведущие "--", если они были
+    evs = {key: val.replace("-", '_').lstrip('_').lower() for key, val in os.environ.items()}
+    flake_parms = [
+        '--show-source',
+        '--jobs=1',
+        '--isolated',
+        '--format=flake8ejudgeformatter',
+    ]
+    if 'flake8_ignore' in evs:
+        flake_parms.append('--ignore=' + evs['flake8_ignore'])
+    else:
+        flake_parms.append('--ignore=' + IGNORE)
 
-# Теперь вычитываем дополнительные настройки
-for i in range(1, 30):
-    pattern, in_or_not, msg = 'chk_pattern_' + str(i), 'chk_in_or_not_' + str(i), 'chk_err_msg_' + str(i)
-    check = [evs.get(x, '') for x in (pattern, in_or_not, msg)]
-    if not all(check):
-        continue
-    pattern, in_or_not, msg = check
+    if 'max_line_length' in evs:
+        flake_parms.append('--max-line-length=' + evs['max_line_length'])
+    else:
+        flake_parms.append('--max-line-length=' + str(MAX_LEN))
 
-    check_found = bool(re.search(pattern, source))
-    if (check_found and in_or_not != 'in') or (not check_found and in_or_not != 'not'):
-        sys.stderr.write(msg)
-        sys.stderr.write('\n')
+    if 'max-complexity' in evs:
+        flake_parms.append('--max-complexity=' + evs['max_line_length'])
+    else:
+        flake_parms.append('--max-complexity=' + str(MAX_COMPLEXITY))
+
+    max_errors = evs.get('max_errors_to_show', MAX_ERRORS_TO_SHOW)
+
+    # Запускаем flake
+    pr = subprocess.Popen([FLAKE] + flake_parms + [src_name],
+                          stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)  # Запускаем процесс
+    stdout_data_b, stderr_data_b = pr.communicate(timeout=TIMEOUT)  # Передаём данные в StdIN
+    if stderr_data_b:
+        f_obj.write('flake8 error:\n' + stderr_data_b.decode('utf-8', 'ignore'))
+        return 1
+    if stdout_data_b:
+        stdout_data = '\n' + stdout_data_b.decode('utf-8', 'ignore')
+        tot_errors = stdout_data.count('\n' + LINE_STARTER)
+        if tot_errors > max_errors:
+            ps = -1
+            for __ in range(max_errors):
+                ps = stdout_data.find('\n' + LINE_STARTER, ps + 1)
+            stdout_data = stdout_data[:ps]
+            rem = tot_errors - max_errors
+            tail = 'ий' if 5 <= rem % 100 <= 20 else 'ия' if 1 < rem % 10 < 5 else 'ие'
+            add = '\n' + '=' * 50 + \
+                  'Там ещё {} замечан{} к стилю.'.format(rem, tail) + \
+                  'Используйте автоформатирование кода: Ctrl+Alt+L в PyCharm или сервис https://black.now.sh'
+            stdout_data += add
         errors_found = True
+        f_obj.write(stdout_data)
 
-# Выходим с ошибкой или без в зависимости от.
-if errors_found:
-    sys.exit(1)
+    # Читаем исходники
+    try:
+        with open(src_name, 'r', encoding="utf-8", errors='ignore') as f:
+            source = f.read()
+    except:
+        f_obj.write('Can not parse source with encoding utf-8')
+        return 1
+
+    # Теперь вычитываем дополнительные настройки
+    for i in range(1, 30):
+        pattern, in_or_not, msg = 'chk_pattern_' + str(i), 'chk_in_or_not_' + str(i), 'chk_err_msg_' + str(i)
+        check = [evs.get(x, '') for x in (pattern, in_or_not, msg)]
+        if not all(check):
+            continue
+        pattern, in_or_not, msg = check
+
+        check_found = bool(re.search(pattern, source))
+        if (check_found and in_or_not != 'in') or (not check_found and in_or_not != 'not'):
+            f_obj.write(msg)
+            f_obj.write('\n')
+            errors_found = True
+
+    # Выходим с ошибкой или без в зависимости от.
+    if errors_found:
+        return 1
+
+
+if __name__ == '__main__':
+    # ejudge вызывает валидатор с единственным параметром — именем файла, которому необходима проверка.
+    src_name = sys.argv[1]
+    exit_code = main(src_name, f_obj=sys.stderr)
+    sys.exit(exit_code)
--- flake8ejudgeformatter.py
+++ flake8ejudgeformatter.py
@@ -1,10 +1,21 @@
 from flake8.formatting import base
+from ru_errors import *
+
+LINE_STARTER = '>Line '
 
 _SEPS = ['"', '(', ')', "'"]
-_ERROR_FORMAT = 'ej:{row}:{col}: {code} {text} ({rutext})'
+_ERROR_FORMAT = LINE_STARTER + '{row}: {code} '
+
 
 def _extract_template(code, text):
     seps = [(i, c) for i, c in enumerate(text) if c in _SEPS]
+    # Если у нас один сепаратор внутри других сепараторов, то он — не сепаратор.
+    for i in range(len(seps) - 2, 0, -1):
+        i1, c1 = seps[i - 1]
+        i2, c2 = seps[i]
+        i3, c3 = seps[i + 1]
+        if i1 + 1 == i2 == i3 - 1 and c1 == c3 != c2:
+            seps.pop(i)
     replacers = []
     if seps:
         if len(seps) % 2 != 0:
@@ -15,21 +26,32 @@ def _extract_template(code, text):
             for i in range(len(seps) - 2, -1, -2):
                 ff = seps[i][0]
                 tt = seps[i + 1][0]
-                text = text[:ff] + '$' + str(i // 2) + text[tt + 1:]
+                text = text[:ff] + '{' + str(i // 2) + '}' + text[tt + 1:]
     if code == 'F811':  # redefinition of unused $0 from line 273
         rsp = text.rfind(' ')
         replacers.append(text[rsp + 1:])
-        text = text[:rsp + 1] + '$1'
+        text = text[:rsp + 1] + '{1}'
     elif code in (
-    'E301', 'E302', 'E305', 'E306'):  # expected 2 blank lines after class or function definition, found
+            'E301', 'E302', 'E305', 'E306'):  # expected 2 blank lines after class or function definition, found
         rsp = text.rfind(' ')
         replacers.append(text[rsp + 1:])
-        text = text[:rsp + 1] + '$0'
+        text = text[:rsp + 1] + '{0}'
     elif code == 'F405':  # $0 may be undefined, or defined from star imports:
         rsp = text.rfind('imports:')
         replacers.append(text[rsp + 9:])
-        text = text[:rsp + 9] + '$1'
+        text = text[:rsp + 9] + '{1}'
+    elif code == 'E712':  # comparison to False should be {0} or {1}
+        if 'False' in text:
+            text = text.replace('False', '{2}')
+            replacers.append('False')
+        elif 'True' in text:
+            text = text.replace('True', '{2}')
+            replacers.append('True')
+
     return text, replacers
+
+
+_extract_template('E211', "whitespace before '('")
 
 
 class Flake8EjudgeFormatter(base.BaseFormatter):
@@ -41,6 +63,21 @@ class Flake8EjudgeFormatter(base.BaseFormatter):
         path = error.filename
         row = error.line_number
         col = error.column_number
-        text, replacers = _extract_template(code, text)
-        rutext = ''
-        return _ERROR_FORMAT.format(**locals())
+        text_r, replacers = _extract_template(code, text)
+        if code in code_to_ru_text:
+            try:
+                rutext = code_to_ru_text[code].format(*replacers)
+            except:
+                rutext = code_to_ru_text[code]
+        elif text_r in text_to_ru_text:
+            try:
+                rutext = text_to_ru_text[text_r].format(*replacers)
+            except:
+                rutext = text_to_ru_text[text_r]
+        else:
+            rutext = ''
+        st = _ERROR_FORMAT.format(**locals())
+        res = st + text
+        if rutext:
+            res += '\n' + ' ' * len(st) + rutext
+        return res
